a10d6f292b5e0b02457840505bc3d983920be26f
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2025-05-25 14:38:47 -0700

    add leap

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,4 +9,12 @@ return {
     event = 'VeryLazy',
     opts = {},
   },
+  {'ggandor/leap.nvim',
+    dependencies = {
+      "tpope/vim-repeat",
+    },
+    config = function()
+      require('leap').add_default_mappings()
+    end
+  }
 }
